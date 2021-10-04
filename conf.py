6ee414d5f2509000a0819e9929fbc42156780414
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2021-10-04 19:26:06 +0000

    woah

--- conf.py
+++ conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'testing'
+project = 'Testing'
 copyright = '2021, Dragonlord1005'
 author = 'Dragonlord1005'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,7 +30,7 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages', 'sphinxext.opengraph'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +47,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+opg_site_url = 'https://dragonlord1005.github.io/spersonal-sphinx-test'
