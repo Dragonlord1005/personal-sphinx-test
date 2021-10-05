b24bf4aed3c43b3bc974da878ed28753aed30d49
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2021-10-05 16:55:17 +0000

    removed opengraph for right now and removed an extension

--- conf.py
+++ conf.py
@@ -30,8 +30,9 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages', 'sphinxext.opengraph', 'myst_parser', 'sphinx.ext.todo'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages','myst_parser', 'sphinx.ext.todo'
 ]
+# sphinxext.opengraph 
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
