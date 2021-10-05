7ecc96679730fa658564e99ee2a4b3b2a023671c
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2021-10-05 16:59:44 +0000

    readded opengraph

--- conf.py
+++ conf.py
@@ -30,9 +30,8 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages','myst_parser', 'sphinx.ext.todo'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages','myst_parser', 'sphinx.ext.todo', 'sphinxext.opengraph'
 ]
-# sphinxext.opengraph 
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
